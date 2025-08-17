79330a5c9275da3654779d191ba84668912209b4
lukabagash <bagash_l2@denison.edu>
2025-08-17 16:51:13 -0400

    Update CLI usage instructions and enrichEmail call to accept firstName and lastName

--- src/cli.ts
+++ src/cli.ts
@@ -2,11 +2,15 @@
 import { enrichEmail } from "./index";
 
 const email = process.argv[2];
+const firstName = process.argv[3];
+const lastName = process.argv[4];
+
 if (!email) {
-  console.error("Usage: social-from-email <email>");
+  console.error("Usage: social-from-email <email> [firstName] [lastName]");
+  console.error("Example: social-from-email luka.yep@gmail.com Luka Bagashvili");
   process.exit(1);
 }
 
-enrichEmail(email)
+enrichEmail(email, { firstName, lastName })
   .then(r => console.log(JSON.stringify(r, null, 2)))
   .catch(e => { console.error(e.message || String(e)); process.exit(1); });
